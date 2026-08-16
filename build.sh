ec0f11c9be2355c8c215535d7b686b3ea1f49d8c
tlemtv098-dotcom <tlemtv098@gmail.com>
2026-08-16 15:25:53 +0700

    Modify build.sh to delete and recreate admin user

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', '1234')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin', 'admin@example.com', '1234')"
